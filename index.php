921f6331bfe06dfb18d13d5833ac46fb1f5c0ec1
bielzinhopecci <gaah.pecci@icloud.com>
2018-05-07 19:44:54 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Search Sound</title>
+
+    <?php include 'includes/link.php'; ?> 
+
+  </head>
+
+<body>
+    
+    
+    <header>
+       <?php 
+        $pagina='index';
+        include 'includes/menu.php'; ?> 
+    </header>
+
+<div class="container">
+        <div class="row">
+            <div class="carroul-l col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <a href="#">
+                <img src="dist/img/rock-main.png" alt="Rock" class="img-animated">
+            </a>
+            </div>
+    
+            <div class="carroul-r col-lg-6 col-md-6 col-sm-12 col-xs-12 toggle-img">
+                <a href="#">
+                    <img src="dist/img/rock-second.jpg" alt="Rock" class="img-animated">
+                </a>
+                    <div class="carroul-rd">
+                            <a href="#">
+                        <img src="dist/img/rock-three.jpg" alt="Rock" class="img-animated">
+                        </a>
+                    </div>
+            </div>
+        </div>
+    <hr class="prop">
+</div>
+
+<div class="container">
+    <h2 class="text-dark">História</h2>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <img src="dist/img/rock-1.jpg" alt="Instrumentos">
+                    <p class="py-2">Nunc hendrerit lectus in purus imperdiet, quis condimentum dolor lobortis. 
+                        In varius, erat id interdum auctor, nisi ex consequat mi, 
+                        ut aliquet felis lectus sit amet orci. <a href="rock.html">Veja mais...</a>
+                    </p>
+            </div>
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                        <img src="dist/img/rock-3.jpg" alt="Instrumentos">
+                        <p class="py-2">Nunc hendrerit lectus in purus imperdiet, quis condimentum dolor lobortis. 
+                                In varius, erat id interdum auctor, nisi ex consequat mi, 
+                                ut aliquet felis lectus sit amet orci. <a href="#">Veja mais...</a>
+                        </p>
+                    </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <img src="dist/img/rock-3.jpg" alt="Instrumentos"> 
+                <p class="py-2">Nunc hendrerit lectus in purus imperdiet, quis condimentum dolor lobortis. 
+                        In varius, erat id interdum auctor, nisi ex consequat mi, 
+                        ut aliquet felis lectus sit amet orci. <a href="#">Veja mais...</a>
+                </p>
+            </div>
+        </div>
+    <hr class="prop">
+</div>
+
+<div class="container">
+        <h2 class="text-dark">Instrumentos</h2>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-3">
+                    <img src="dist/img/rock.jpg" alt="Instrumentos"> 
+                </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-3">
+                            <img src="dist/img/samba.jpg" alt="Instrumentos"> 
+                        </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-3">
+                    <img src="dist/img/pagode.jpg" alt="Instrumentos"> 
+                </div>
+            </div>
+        <hr class="prop">
+    </div>
+
+        <footer>
+       <?php include 'includes/footer.php'; ?> 
+        </footer>
+
+
+       <?php include 'includes/script.php'; ?> 
+  </body>
+
+</html>
---
+++ quemsomos.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Search Sound</title>
+
+    <?php include 'includes/link.php'; ?> 
+
+  </head>
+
+<body>
+
+<header>
+    <?php 
+        $pagina='quemsomos';
+        include 'includes/menu.php'; ?> 
+    </header>
+
+    
+<section class="container">
+    
+        <h1 class="display-4 text-dark my-5 text-center">Quem Somos</h1>
+    <div class="row">
+        <div class="col-md-2">
+            <img src="dist/img/img-grupo.jpg" alt="Foto Grupo">
+        </div>
+        <div class="col-md-10 textgrupo">
+            <p>Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
+        </div>
+    </div>
+        <div class="row mt-3 mb-3">
+        <div class="col-md-2">
+            <img src="dist/img/img-grupo.jpg" alt="Foto Grupo">
+        </div>
+        <div class="col-md-10 textgrupo">
+            <p>Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
+        </div>
+    </div>
+        <div class="row mb-5">
+        <div class="col-md-2">
+            <img src="dist/img/img-grupo.jpg" alt="Foto Grupo">
+        </div>
+        <div class="col-md-10 textgrupo">
+            <p>Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
+        </div>
+    </div>
+    
+</section>
+
+
+        <footer>
+       <?php include 'includes/footer.php'; ?> 
+        </footer>
+
+
+<?php include 'includes/script.php'; ?> 
+
+</body>
+</html>
