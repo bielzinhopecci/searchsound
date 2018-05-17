dcb92a0261c1a36394743da27d263b28072fca8e
Felipe Ribeiro <33236382+DrkNNNN@users.noreply.github.com>
2018-05-17 08:10:57 -0300

    Add files via upload

--- rock-inst.php
+++ rock-inst.php
@@ -23,42 +23,82 @@
     </header>
 
 <section class="container">
-  <h1 class="text-center pt-3 pb-5">Instrumentos do Rock 'n' Roll</h1>
-    <div class="row pb-5">
-      <div class="col-md-4 col-xs-12">
-        <img class="icon-instrumentos pb-1" src="dist/img/icones-rock/guitar.png" alt="Guitarra">
-          <h2 class="text-center pt-3 mt-1">Guitarra</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-      </div>
-      <div class="col-md-4 col-xs-12">
-        <img class="icon-instrumentos" src="dist/img/icones-rock/drums.png" alt="Bateria">
-          <h2 class="text-center pt-2">Bateria</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-      </div>
-      <div class="col-md-4 col-xs-12">
-        <img class="icon-instrumentos" src="dist/img/icones-rock/violao.png" alt="Violão">
-          <h2 class="text-center pt-2">Violão</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+  <h1 class="text-center pt-5 mt-3 pb-5">Instrumentos do Rock 'n' Roll</h1>
+    <div class="row pb-1">
+    <div class="lista-item border-middle py-5 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <input class="trigger-input" id="faq-titulo-1" type="checkbox">
+		      <div class="trigger-wrapper">
+            <img class="icon-instrumentos pb-1" src="dist/img/icones-rock/guitar.png" alt="Guitarra">
+            <h2 class="text-center mt-2 pb-4">Guitarra</h2>
+          <label for="faq-titulo-1">
+            <h6 class="faq-titulo">Descrição</h6>
+          </label>
+          <p class="faq-conteudo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
       </div>
     </div>
+
+    <div class="lista-item border-middle py-5 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+      <input class="trigger-input" id="faq-titulo-2" type="checkbox">
+		    <div class="trigger-wrapper">
+          <img class="icon-instrumentos" src="dist/img/icones-rock/drums.png" alt="Bateria">
+          <h2 class="text-center">Bateria</h2>
+        <label for="faq-titulo-2">
+          <h6 class="faq-titulo">Descrição</h6>
+        </label>
+        <p class="faq-conteudo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+      </div>
+    </div>
+
+    <div class="lista-item border-middle py-5 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+      <input class="trigger-input" id="faq-titulo-3" type="checkbox">
+		    <div class="trigger-wrapper">
+          <img class="icon-instrumentos" src="dist/img/icones-rock/violao.png" alt="Violão">
+          <h2 class="text-center">Violão</h2>
+        <label for="faq-titulo-3">
+          <h5 class="faq-titulo">Descrição</h5>
+        </label>
+        <p class="faq-conteudo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+      </div>
+    </div>
+  </div>
     
-        <div class="row">
-        <div class="col-md-4">
-            <img class="icon-instrumentos" src="dist/img/icones-rock/piano.png" alt="Piano">
-              <h2 class="text-center pt-4 mt-1">Piano</h2>
-              <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-          </div>
-          <div class="col-md-4">
-            <img class="icon-instrumentos" src="dist/img/icones-rock/bass.png" alt="Baixo">
-            <h2 class="text-center">Baixo</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-          </div>
-          <div class="col-md-4">
-            <img class="icon-instrumentos" src="dist/img/icones-rock/teclado.png" alt="Teclado">
-            <h2 class="text-center pt-4 mt-1">Teclado</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
-          </div>
-        </div>
+  <div class="row">
+    <div class="lista-item border-middle py-5 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <input class="trigger-input" id="faq-titulo-4" type="checkbox">
+		      <div class="trigger-wrapper">
+            <img class="icon-instrumentos pb-1" src="dist/img/icones-rock/piano.png" alt="Piano">
+            <h2 class="text-center mt-2 pb-4">Piano</h2>
+          <label for="faq-titulo-4">
+            <h6 class="faq-titulo">Descrição</h6>
+          </label>
+          <p class="faq-conteudo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+      </div>
+    </div>
+
+    <div class="lista-item border-middle py-5 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+      <input class="trigger-input" id="faq-titulo-5" type="checkbox">
+		    <div class="trigger-wrapper">
+          <img class="icon-instrumentos" src="dist/img/icones-rock/bass.png" alt="Baixo">
+          <h2 class="text-center">Baixo</h2>
+        <label for="faq-titulo-5">
+          <h6 class="faq-titulo">Descrição</h6>
+        </label>
+        <p class="faq-conteudo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+      </div>
+    </div>
+
+    <div class="lista-item border-middle py-5 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+      <input class="trigger-input" id="faq-titulo-6" type="checkbox">
+		    <div class="trigger-wrapper">
+          <img class="icon-instrumentos" src="dist/img/icones-rock/teclado.png" alt="Teclado">
+          <h2 class="text-center">Teclado</h2>
+        <label for="faq-titulo-6">
+          <h5 class="faq-titulo">Descrição</h5>
+        </label>
+        <p class="faq-conteudo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, nemo nulla.</p>
+      </div>
+    </div>
+  </div>
 </section>
 
 <section class="container">
