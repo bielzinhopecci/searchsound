a63bd2440630f5011500f16d95b52dcf424e90ea
Felipe Ribeiro <33236382+DrkNNNN@users.noreply.github.com>
2018-05-17 21:31:32 -0300

    Delete link.php

--- searchsound-master/includes/link.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link href="dist/css/style.css" rel="stylesheet">
-<link href="dist/css/rock.css" rel="stylesheet">
-<link href="dist/css/rock-inst.css" rel="stylesheet">
